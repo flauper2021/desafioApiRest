ccdebf0e0cf834a837bf369221a0ba1cf5c8acee
mauricioJuica <mauricio.work@yahoo.com>
2023-10-21 11:01:21 -0300

    atualização do projeto, criado camada de dominio .

---
+++ santander-dev/src/main/java/dio/santanderdev/domain/model/Feature.java
@@ -0,0 +1,10 @@
+package dio.santanderdev.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem{
+
+
+
+}
---
+++ santander-dev/src/main/java/dio/santanderdev/domain/model/News.java
@@ -0,0 +1,11 @@
+package dio.santanderdev.domain.model;
+
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
+
+
+
+}
